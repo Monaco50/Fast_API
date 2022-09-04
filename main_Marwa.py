21d9c4f1a6e3d5c7182b4e89c64c2056f3acdbe5
Monaco50 <112497942+Monaco50@users.noreply.github.com>
2022-09-04 22:21:25 +0200

    create index.md
    to host api on github

---
+++ main_Marwa.py
@@ -0,0 +1,50 @@
+# APP FastAPI
+# API renvoie le score d’un client (moteur d’inférence)
+
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
+from sklearn.metrics import roc_auc_score, average_precision_score, \
+    fbeta_score, make_scorer
+from pydantic import BaseModel
+import joblib
+from models_core_Marwa import *
+
+
+# 2. Create the app and model objects
+api = FastAPI(
+    title="LGBM Prediction",
+    description="Predict score",
+    version="1.0.0")
+
+model = Predict()
+
+
+@api.get("/Marwa")
+async def current_user():
+    result = {
+            'status' : 'success',
+            'message' : (f"Hello ! Welcome to Fast API.")
+        }
+    return result
+
+# 3. Prediction
+
+@api.post('/predict')
+async def make_predictions():
+    """
+    Effectuer des prédictions sur de nouvelles données en provenance d'un fichier pickle
+    """
+    
+    return model.predict()
+
+
+
+
+
+
---
+++ models_core_Marwa.py
@@ -0,0 +1,22 @@
+# models_core.py
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.metrics import classification_report, balanced_accuracy_score, accuracy_score
+
+class Predict:
+    # Class constructor, loads the dataset and loads the model and predict
+
+    @staticmethod
+    def predict():
+        lgbm_model = pickle.load(open('best_model.pickle', 'rb'))
+        test_set = pickle.load(open('test_fs_lightgbm_80.pickle', 'rb'))
+        
+        X_test = test_set.drop('SK_ID_CURR', axis=1)
+        probability = lgbm_model.predict_proba(X_test)[:, 1] #probabilité
+        prediction = lgbm_model.predict(X_test) #prediction
+     
+        output = pd.DataFrame({'prediction': prediction[:10],  'probability': probability[:10]})
+     
+        return output.to_dict(orient = 'records')
+       
